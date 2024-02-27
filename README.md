56b670f3c94ee771232a834e22519771d80d8f1a
laurelis <s23kairlaur@venta.lv>
2024-02-27 20:10:02 +0200

    last update

--- README.md
+++ README.md
@@ -1,6 +1,9 @@
 # Web izstrāde 1.darbs
+
 Lauris Kairo
-Programmēšananas speciālists 
+Programmēšananas speciālists
 1.kurss
 Ventspils
 Mazs html, css, javascript projekts
+
+The content provided is for educational and informational purpose only!
--- script.js
+++ script.js
@@ -6,35 +6,48 @@ const leftGaleryBtn = document.querySelector(".btn-left");
 const rightGaleryBtn = document.querySelector(".btn-right");
 const imageContainer = document.querySelector(".image-container");
 const galeryImg = document.getElementById("galery-img");
-const imageGalery = document.querySelector(".image-galery-container");
+const imageGalery = document.querySelector(".pictures-container");
 const factText = document.querySelector(".fact");
 const video = document.querySelector(".video");
 const videoLoadingScreen = document.querySelector(".video-loading-screen");
-const scrollUpBtn = document.querySelector(".btn-scroll-up")
-video.onloadeddata = e => {
-  setInterval(() => {
-    videoLoadingScreen.classList.add("hide");
-  }, 1500);
-};
+const scrollUpBtn = document.querySelector(".btn-scroll-up");
+const audioBtn = document.querySelector(".btn-tiger-roar");
+const audioTigerRoar = document.getElementById("audio-tiger-roar");
+
+/// settings
+const INTERVAL_BETWEEN_FACTS_MS = 5000;
+const AUDIO_VOLUME = 20;
 
 let imgIdx = 0;
 let factIdx = 0;
 let previousScrollPos = window.scrollY;
 let timeOut = 0;
-
+let audioClicked = false;
 
 const tigerFacts = [
-  "Tīģera acis ir apaļas atšķirībā no mājas kaķiem. Kam tās ir mazliet sašaurinātas. Tas tāpēc, ka citi kaķi labāk medi naktīs, bet tīģeri krēslas laikā, galvanokārt no rīta un vakarā.",
-  "Tīģeri neredz naktīs tik labi ,ka daudzi citi kaķu dzimtes pārstāvji, tīģerim naktī redze ir aptuveni 6 reizes labāka nekā cilvēkam.",
+  "Tīģera acis ir apaļas atšķirībā no mājas kaķiem, kam tās ir mazliet sašaurinātas. Tas tāpēc, ka citi kaķi labāk medī naktīs, bet tīģeri krēslas laikā, galvenokārt no rīta un vakarā.",
+  "Tīģeri neredz naktīs tik labi kā daudzi citi kaķu dzimtes pārstāvji. Tīģerim naktī redze ir aptuveni 6 reizes labāka nekā cilvēkam.",
   "Vairums tīģeru ir dzeltanas acis, bet baltajiem tīģeriem tās ir zilas.",
   "Katra tīģera svītras ir unikālas tā pat kā cilvēka pirkstu nospiedumi. Nav tīģeru ar vienādu kažoku.",
   "Tīģera pieres marķējums līdzinās ķīniešu heroglifam, ko apzīmē kā karalis. Tas dod tīģerim kulta reliģisko statusu.",
-  "Atšķirībā gandrīz no visiem lielajiem kaķiem tīģeri ir lieliski peldētāji. Viņi bauda peldēšanu un bieži spēlējās ūdenī. Pieauguši tīģeri var nopeldēt vairākus kilometrus ,lai piekļūtu medījumam, viņi bez problēmām šķerso upes. Garākais reģistrētais tīģera peldējums ir mazliet vairāk kā 30 km.",
-  "Tīģeri ir pilnīgi akli savas dzīves pirmajā nedēļā. Apēram puse tīģeru neizdzīvo līdz pilngadībai.",
-  "Tīģeris parasti neuzbrūk ,ja medījamais dzīvnieks viņu jau ir ieraudzījis, jo tad zūd pārsteiguma moments.",
-  "Tīģeri vat atīstīt atrumu virs 60 km/h , bet tikai nelielos attālumos.",
+  "Atšķirībā gandrīz no visiem lielajiem kaķiem tīģeri ir lieliski peldētāji. Viņi bauda peldēšanu un bieži spēlējās ūdenī. Pieauguši tīģeri var nopeldēt vairākus kilometrus, lai piekļūtu medījumam, viņi bez problēmām šķērso upes. Garākais reģistrētais tīģera peldējums ir mazliet vairāk kā 30 km.",
+  "Tīģeri ir pilnīgi akli savas dzīves pirmajā nedēļā.",
+  "Tīģeris parasti neuzbrūk, ja medījamais dzīvnieks viņu jau ir ieraudzījis, jo tad zūd pārsteiguma moments.",
+  "Tīģeri var atīstīt ātrumu virs 60 km/h, bet tikai nelielos attālumos.",
   "Tīģeris var aizlēkt vairāk nekā 6 m tālu un uzlēkt vairāk nekā 5 metru augstumā.",
+  "Apēram puse tīģeru neizdzīvo līdz pilngadībai.",
 ];
+
+window.onload = () => {
+  textFactAnimation();
+  setAudioVolume();
+};
+
+video.onloadeddata = () => {
+  setInterval(() => {
+    videoLoadingScreen.classList.add("hide");
+  }, 1500);
+};
 
 /// EVENTS
 Array.from(imageGalery.children).forEach((img, idx) => {
@@ -44,6 +57,7 @@ exitGaleryBtn.addEventListener("click", closeGaleryModal);
 leftGaleryBtn.addEventListener("click", decreaseImgIdx);
 rightGaleryBtn.addEventListener("click", increaseImgIdx);
 scrollUpBtn.addEventListener("click", scrollUp);
+audioBtn.addEventListener("click", playSound);
 
 window.addEventListener("resize", () => {
   if ((window.innerWidth <= 1000 || window.innerHeight <= 300) && !galeryModal.className.includes("close")) {
@@ -107,56 +121,61 @@ function increaseImgIdx() {
   }, 50);
 }
 
-factIdx++;
+function textFactAnimation(idx = 0) {
+  let fact = tigerFacts[idx];
 
-let sum = (tigerFacts[factIdx].length * tigerFacts[factIdx].length + 1) / 1.8;
-
-textAnimation();
-
-setInterval(async () => {
-  factText.innerHTML = "";
-  if (factIdx >= tigerFacts.length) {
-    factIdx = 0;
-  }
-  factIdx++;
-  textAnimation();
-}, sum + 5000);
-
-function textAnimation() {
-  let fact = tigerFacts[factIdx];
-  //let result = "";
   for (let i = 0; i < fact.length; i++) {
     const spanText = document.createElement("span");
     spanText.classList.add("hide");
     spanText.innerText = fact[i];
     factText.appendChild(spanText);
-    setTimeout(() => {
+    setTimeout(async () => {
       const newSpanText = document.createElement("span");
       newSpanText.innerText = fact[i];
-
-      // factText.replaceChild(spanText, newSpanText);
       spanText.replaceWith(newSpanText);
-      //spanText.innerText = fact[i];
 
-      //console.log(result);
-    }, i * 40);
+      if (i >= fact.length - 1) {
+        await checkPromiseForAnimation();
+        await shrinkAnimation();
+        textFactAnimation(idx >= tigerFacts.length - 1 ? 0 : idx + 1);
+      }
+    }, i * 60);
   }
+}
+
+function shrinkAnimation() {
+  return new Promise(res => {
+    setTimeout(() => {
+      Array.from(factText.children).forEach(char => {
+        char.classList.add("shrink");
+        setTimeout(() => {
+          factText.removeChild(factText.firstChild);
+          res(1);
+        }, 1000);
+      });
+    }, INTERVAL_BETWEEN_FACTS_MS);
+  });
+}
+function checkPromiseForAnimation() {
+  return new Promise(res => {
+    res(1);
+  });
 }
 
 function showHideNavbar() {
   let nextPos = window.scrollY;
+  if (nextPos <= 280) return;
 
   if (nextPos > previousScrollPos) {
-    previousScrollPos = nextPos;
     if (!navbar.className.includes("hide")) {
       navbar.classList.add("hide");
     }
   } else if (nextPos < previousScrollPos) {
-    previousScrollPos = nextPos;
     if (navbar.className.includes("hide")) {
       navbar.classList.remove("hide");
     }
   }
+  previousScrollPos = nextPos;
 }
 
 function hideScrollUpBtn() {
@@ -164,8 +183,25 @@ function hideScrollUpBtn() {
   if (window.scrollY >= half && scrollUpBtn.className.includes("hide")) {
     scrollUpBtn.classList.remove("hide");
   } else if (window.scrollY < half && !scrollUpBtn.className.includes("hide")) {
-    scrollUpBtn.classList.add("hide")
+    scrollUpBtn.classList.add("hide");
   }
+}
+
+function playSound() {
+  if (audioClicked) return;
+  audioClicked = true;
+  if (audioClicked) {
+    setTimeout(() => {
+      audioClicked = false;
+    }, audioTigerRoar.duration * 1000);
+
+    audioTigerRoar.currentTime = 0;
+    audioTigerRoar.play();
+  }
+}
+
+function setAudioVolume() {
+  audioTigerRoar.volume = AUDIO_VOLUME / 100;
 }
 
 function scrollUp() {
